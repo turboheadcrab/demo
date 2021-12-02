dc90f9fd04d2c1eedce4132bcd752766cd373271
Vladimir Apraksin <turbo.headcrab.666@gmail.com>
2021-12-01 20:36:05 -0500

    Redirects and Data

--- src/main/java/com/jrp/demo/web/HomeController.java
+++ src/main/java/com/jrp/demo/web/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -16,7 +17,14 @@ public class HomeController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String processFormData(User user) {
+    public String processFormData(User user, RedirectAttributes attr) {
+        // Insert data submitted in the form to the database
+        attr.addFlashAttribute("user", user);
+        return "redirect:/display";
+    }
+
+    @RequestMapping(value = "/display", method = RequestMethod.GET)
+    public String displayFormData(User user) {
         return "result";
     }
 }
